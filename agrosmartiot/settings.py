4d93ce7fbbdcaae820c3a798cdaa9880e4e792b0
unknown <andy.luarte@gmail.com>
2024-10-01 15:56:27 -0300

    septimo commit

--- agrosmartiot/settings.py
+++ agrosmartiot/settings.py
@@ -92,9 +92,10 @@ import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Ubicación del archivo de la base de datos
+    }
 }
 # settings.py
 AUTHENTICATION_BACKENDS = [
