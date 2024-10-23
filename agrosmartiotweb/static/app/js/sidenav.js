82cde356872a07e3a6ecf9128fe0adeae7fd0123
unknown <andy.luarte@gmail.com>
2024-10-23 14:56:13 -0300

    problemas diseño1

--- agrosmartiotweb/static/app/js/sidenav.js
+++ agrosmartiotweb/static/app/js/sidenav.js
@@ -1,17 +1,23 @@
 function toggleNav() {
     var sidenav = document.getElementById("mySidenav");
+    var menuButton = document.getElementById("menuButton");
+
+    // Si el sidenav tiene width 0, lo abrimos
     if (sidenav.style.width === "0px" || sidenav.style.width === "") {
-        sidenav.style.width = "250px"; // Abrir
+        sidenav.style.width = "250px";
     } else {
-        sidenav.style.width = "0"; // Cerrar
+        // Si no, lo cerramos
+        sidenav.style.width = "0";
     }
 }
 
-// Cerrar el sidenav si se hace clic fuera de él
+// Evitamos que el clic en cualquier parte de la página abra el sidenav
 document.addEventListener('click', function(event) {
     var sidenav = document.getElementById('mySidenav');
     var menuButton = document.getElementById('menuButton');
+
+    // Si el clic no es en el sidenav o en el botón, cerramos el sidenav
     if (!sidenav.contains(event.target) && !menuButton.contains(event.target)) {
-        sidenav.style.width = "0"; // Cerrar
+        sidenav.style.width = "0";
     }
 });
