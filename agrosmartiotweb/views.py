2e27a72bf2abe0945c7102a3b6cd61f27e8ccefc
unknown <andy.luarte@gmail.com>
2025-01-13 18:11:19 -0300

    CAMBIO PARA CUADERNO DE CAMPO

--- agrosmartiotweb/views.py
+++ agrosmartiotweb/views.py
@@ -1598,8 +1598,8 @@ def cuadernodecampo(request):
     for proceso in procesos:
         eventos.append({
             'title': f"Tarea: {proceso.trabajo} ",
-            'start': proceso.fecha.isoformat(),
-            'end': proceso.fecha.isoformat(),
+            'start': proceso.fecha_compra.isoformat(),
+            'end': proceso.fecha_compra.isoformat(),
             
             'trabajador': proceso.asignado.nombre,
         })
