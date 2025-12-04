f36001edaf75e7000536a25de5ac5d700ffea11f
Andrés Luarte <andy.luarte@gmail.com>
2025-12-04 16:17:20 -0300

    Correccion 1

--- agrosmartiot/settings.py
+++ agrosmartiot/settings.py
@@ -40,7 +40,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne'
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
